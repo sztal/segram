48b456dcdee57db623b9246d0c426ead36b86679
Szymon Talaga <stalaga@protonmail.com>
2023-09-23 13:44:10 +0200

    Treat 'Graph' class as an implementation detail of grammar

--- segram/grammar/graph.py
+++ segram/grammar/graph.py
@@ -1,6 +1,169 @@
 from __future__ import annotations
-from typing import Any, Self
-from ..utils.types import Graph
+from typing import Any, Iterable, Iterator, Mapping, MutableMapping, Self
+from graphlib import TopologicalSorter, CycleError
+
+
+class Graph(MutableMapping):
+    """Graph.
+
+    By default it has a form of a mapping from sources to targets,
+    but the order can be easily reversed using :attr:`~rev` property.
+
+    Notes
+    -----
+    Only hashable objects can used as nodes.
+    """
+    __slots__ = ("_data", "_is_dag", "_rev")
+
+    def __init__(self, data: Mapping[Any, tuple[Any, ...]]) -> None:
+        self._data = data
+        self._is_dag = None
+        self._rev = None
+
+    def __repr__(self) -> str:
+        return repr(self.data)
+
+    def __iter__(self) -> Iterator[Any]:
+        yield from self.data
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, key: Any) -> Iterable[Any]:
+        return self.data[key]
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        self.data[key] = value
+
+    def __delitem__(self, key: Any) -> None:
+        del self.data[key]
+
+    # Properties --------------------------------------------------------------
+
+    @property
+    def data(self) -> dict[Any, tuple[Any, ...]]:
+        return self._data
+
+    @property
+    def links(self) -> Iterator[tuple[Any, Any]]:
+        for parent, children in self.items():
+            for child in children:
+                yield parent, child
+
+    @property
+    def rev(self) -> Graph:
+        if self._rev is None:
+            self._rev = self.get_rev()
+        return self._rev
+
+    @property
+    def sorted(self) -> Graph:
+        return self.__class__({
+            k: tuple(sorted(v))
+            for k, v in sorted(self.items(), key=lambda x: x[0])
+        })
+
+    @property
+    def sources(self) -> tuple[Any, ...]:
+        pmap = self.rev
+        src = list(self.isolates)
+        for parent in self.toposort().static_order():
+            if pmap.get(parent):
+                break
+            src.append(parent)
+        return tuple(sorted(src))
+
+    @property
+    def isolates(self) -> tuple[Any, ...]:
+        isol = []
+        rev = self.rev
+        for node in self:
+            if not self.get(node) and not rev.get(node):
+                isol.append(node)
+        return tuple(isol)
+
+    @property
+    def is_dag(self) -> bool:
+        if self._is_dag is None:
+            try:
+                self.toposort().prepare()
+                self._is_dag = True
+            except CycleError:
+                self._is_dag = False
+        return self._is_dag
+
+    # Methods -----------------------------------------------------------------
+
+    def get_rev(self) -> Graph:
+        """Get reversed graph (mapping children to parent sets)."""
+        graph = {}
+        for parent, children in self.items():
+            if parent not in graph:
+                graph[parent] = []
+            for child in children:
+                graph.setdefault(child, []).append(parent)
+        return self.__class__(graph).sorted
+
+    def update_rev(self) -> None:
+        """Update reversed graph."""
+        self._rev = self.get_rev()
+
+    def is_comparable_with(self, other: Mapping) -> bool:
+        return isinstance(other, Mapping)
+
+    def toposort(self, *, reversed: bool = False) -> TopologicalSorter:
+        """Get topological sorter.
+
+        Parameters
+        ----------
+        reversed
+            Should reversed topological sorter be returned.
+        """
+        # pylint: disable=redefined-builtin
+        return TopologicalSorter(self if reversed else self.rev)
+
+    def iter_hierarchy(self) -> Iterable[tuple[int, Any, Any]]:
+        """Iterate over parents and children according to the implicit
+        directed acyclic graph hierarchy.
+
+        Yields
+        ------
+        depth
+            Depth index.
+        parent, child
+            Parent and child nodes.
+        """
+        def _iter(parent, depth=0):
+            if (children := self.get(parent)):
+                for child in children:
+                    yield depth, parent, child
+                    yield from _iter(child, depth=depth+1)
+            elif depth == 0:
+                yield depth, parent, None
+        for source in self.sources:
+            yield from _iter(source, depth=0)
+
+    @classmethod
+    def from_links(
+        cls,
+        links: Iterable[tuple[Any, Any]]
+    ) -> Graph:
+        """Construct from links.
+
+        Parameters
+        ----------
+        links
+            Iterable of links (parent-child pairs).
+        """
+        graph = {}
+        for parent, child in links:
+            if parent not in graph:
+                graph[parent] = []
+            if child is not None:
+                graph[parent].append(child)
+                if child not in graph:
+                    graph[child] = []
+        return cls(graph).sorted
 
 
 class PhraseGraph(Graph):
